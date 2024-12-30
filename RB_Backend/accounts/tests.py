793509720d702c20c8d9d4d1a2da03dfcde736b7
Abenet Gebre <mail.micrq@proton.me>
2024-12-30 13:36:32 +0300

    added test for creation of superuser account

--- RB_Backend/accounts/tests.py
+++ RB_Backend/accounts/tests.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 class AccountTests(TestCase):
     """ tests for user model """
-    def test_user_model(self):
+    def test_customuser_model(self):
         """ test user model """
         User = get_user_model()
         user = User.objects.create_user(
@@ -17,6 +17,21 @@ class AccountTests(TestCase):
         self.assertEqual(user.firstname, "test")
         self.assertEqual(user.lastname, "user")
         self.assertFalse(user.is_staff, False)
+
+    def test_superuser_creation(self):
+        """ test superuser creation """
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            email="admin@user.com",
+            firstname="admin",
+            lastname="user",
+            password="pass@123"
+        )
+        self.assertEqual(user.email, "admin@user.com")
+        self.assertEqual(user.firstname, "admin")
+        self.assertEqual(user.lastname, "user")
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
 
 
 class AccountViewsTests(TestCase):
